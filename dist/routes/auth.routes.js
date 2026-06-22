8dcf30e346b1395f7e6432afb72a7d61e6a5a889
telephant11 <telephant11@gmail.com>
2026-06-22 16:52:44 +0400

    chore: untrack dist/ build artifacts (already in .gitignore)

--- dist/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth.controller");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.get('/me', auth_1.authMiddleware, auth_controller_1.getCurrentUser);
-exports.default = router;
-//# sourceMappingURL=auth.routes.js.map
